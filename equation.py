29e51f7ce41996c087d0fe6d8dfe6842aaab48a0
Mohamed Talhaoui <144726758+mohamedtalhaouii@users.noreply.github.com>
2024-01-12 14:51:23 +0100

    add comment

--- equation.py
+++ equation.py
@@ -7,14 +7,14 @@ b = float(Fraction(input("Enter the Second number: ")))
 c = float(Fraction(input("Enter the last number: ")))
 
 Delta = (b ** 2) - 4 * (a * c)
-
+# D = 0
 if Delta == 0 :
    X = -b / (2 * a)
    print("The Solution is : " , "X =",Fraction(X))
-
+# D < 0
 if Delta < 0 :
-   print("No Solution in Ensemble R")   
-
+   print("No Solution in Ensemble R")
+# D > 0
 if Delta > 0 :
    X1 = (-b + sqrt(Delta)) / (2 * a)
    X2 = (-b - sqrt(Delta)) / (2 * a)
